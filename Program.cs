bb613b4b4fb42aa4a63d8500b1eb870a285fca73
Akna2021 <anna.musial@currenda.pl>
2021-06-11 13:56:46 +0200

    samodzielna praca

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace nowyAnnaMusial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Random rand = new Random();
+            Console.WriteLine("Czesc i czolem");
+            for(int i=0;i<10;i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(rand.Next(100,999));
+            }
+        }
+    }
+}
+
